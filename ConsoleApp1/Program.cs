c1fb2342f9539be70a328abc0c6f574dd7b6fbd9
Abdelrahman Ghrayeb <hamza@logicteca.com>
2023-01-30 23:53:43 +0200

    event

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Events;
+
+Console.WriteLine("Hello, World!");
+var stock = new Stock("Amazon");
+stock.Price = 100m;
+
+//Console.WriteLine($"{stock.Name} stock price befor change: ${stock.Price}");
+//stock.ChangeStockPriceBy(0.05m);
+//Console.WriteLine($"{stock.Name} stock price after change: ${stock.Price}");
+//subscribe to event
+stock.OnStockChanged += Stock_OnStockChanged;
+//stock.OnStockChanged += (Stock stock, decimal oldPrice) =>
+//{
+//    Console.ForegroundColor = ConsoleColor.Black;
+
+//    var result = "";
+//    if (stock.Price > oldPrice)
+//    {
+
+
+//        Console.ForegroundColor = ConsoleColor.Green;
+//        result = "Up";
+//    }
+//    else if (stock.Price < oldPrice)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        result = "Down";
+//    }
+//    else
+//        Console.ForegroundColor = ConsoleColor.Gray;
+//    Console.WriteLine($"{stock.Name} stock price : ${stock.Price} - {result}");
+
+//};
+
+stock.ChangeStockPriceBy(0.05m);
+stock.ChangeStockPriceBy(-0.02m);
+stock.ChangeStockPriceBy(0.00m);
+
+//unsubscribe
+stock.OnStockChanged -= Stock_OnStockChanged;
+
+void Stock_OnStockChanged(Stock stock, decimal oldPrice)
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+
+    var result = "";
+    if (stock.Price > oldPrice)
+    {
+
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        result = "Up";
+    }
+    else if (stock.Price < oldPrice)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        result = "Down";
+    }
+    else
+        Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine($"{stock.Name} stock price : ${stock.Price} - {result}");
+}
+
+Console.ReadKey();
---
+++ ConsoleApp1/Stock.cs
@@ -0,0 +1,34 @@
+﻿namespace Events
+{
+    internal class Stock
+    {
+
+        public delegate void StockChangeHandler(Stock stock, decimal oldPrice);
+        //event type like delegate name
+        public event StockChangeHandler OnStockChanged;
+        private string _name;
+        private decimal _price;
+
+        public string Name => this._name;
+        public decimal Price { get { return this._price; } set { this._price = value; } }
+
+        public Stock(string name) => this._name = name;
+
+        public void ChangeStockPriceBy(decimal percent)
+        {
+            var oldPrice = this._price;
+            this._price += Math.Round(this._price * percent);
+            if (OnStockChanged != null)//make sure subscriber exist
+            {
+                OnStockChanged(this, oldPrice);//fire event
+            }
+        }
+
+    }
+}
+/*
+ * 1- delegate with void return take class type & parameters you need to change
+ * 2-public event with type of delgate;
+ * 3-make sure there is subscriber exist
+ * 4-on method you need to fire event fire event
+ * */
