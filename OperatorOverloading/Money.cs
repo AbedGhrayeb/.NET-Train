744fa1dfbf7045b92880fb8a5e6fb689e6b62db4
Abdelrahman Ghrayeb <hamza@logicteca.com>
2023-01-31 19:41:09 +0200

    operator overloading

---
+++ OperatorOverloading/Money.cs
@@ -0,0 +1,50 @@
+﻿namespace OperatorOverloading
+{
+    internal class Money
+    {
+        private decimal _amount;
+        public decimal Amount { get { return _amount; } }
+
+        public Money(decimal amount)
+        {
+            _amount = amount;
+        }
+        public static Money operator +(Money money1, Money money2)
+        {
+            var result = money1.Amount + money2.Amount;
+            return new Money(result);
+        }
+        public static Money operator -(Money money1, Money money2)
+        {
+            var result = money1.Amount - money2.Amount;
+            return new Money(result);
+        }
+        public static Money operator *(Money money1, Money money2)
+        {
+            var result = money1.Amount * money2.Amount;
+            return new Money(result);
+        }
+        public static Money operator /(Money money1, Money money2)
+        {
+            var result = money1.Amount / money2.Amount;
+            return new Money(result);
+        }
+        public static bool operator >(Money money1, Money money2) => money1.Amount > money2.Amount;
+        public static bool operator <(Money money1, Money money2) => money1.Amount < money2.Amount;
+        public static bool operator >=(Money money1, Money money2) => money1.Amount >= money2.Amount;
+        public static bool operator <=(Money money1, Money money2) => money1.Amount <= money2.Amount;
+        public static bool operator ==(Money money1, Money money2) => money1.Amount == money2.Amount;
+        public static bool operator !=(Money money1, Money money2) => money1.Amount != money2.Amount;
+        public static Money operator ++(Money money)
+        {
+            var result = money.Amount;
+            return new Money(++result);
+        }
+        public static Money operator --(Money money)
+        {
+            var result= money.Amount;
+            return new Money(--result);
+
+        }
+    }
+}
---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OperatorOverloading;
+
+Console.WriteLine("Hello, World!");
+var money1 = new Money(20m);
+var money2 = new Money(10m);
+var money=money1+ money2;
+Console.WriteLine($"amount +: {money.Amount}");
